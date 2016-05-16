9e53bdbdd3b10cd96b05d9ebfcab7025d09cd860
Maximilian Wright <twixthehero@gmail.com>
2016-05-16 18:03:42 -0400

    Initial project commit with base client class

---
+++ Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UDP
+{
+    public class Client
+    {
+        private string _ip;
+        private int _port;
+
+        public Client()
+        {
+            IsConnected = false;
+        }
+
+        public bool IsConnected { get; protected set; }
+
+        public void Connect(string ip, int port)
+        {
+
+        }
+
+        public void Disconnect()
+        {
+
+        }
+
+        public void Send(byte[] buffer)
+        {
+
+        }
+    }
+}
