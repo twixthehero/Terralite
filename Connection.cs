33d4260e9b3e74b82e4f945876cc52d85138ad4d
Maximilian Wright <twixthehero@gmail.com>
2016-05-21 14:17:11 -0400

    Migrated to the connection structure

--- Connection.cs
+++ Connection.cs
@@ -1,43 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Timers;
 
 namespace Terralite
 {
     /// <summary>
-    /// 
+    /// This class holds data for a network connection to a single end point.
     /// </summary>
     public class Connection
     {
+        /// <summary>
+        /// Where this connection is connected to
+        /// </summary>
         public EndPoint EndPoint { get; private set; }
+
+        /// <summary>
+        /// Connection ID
+        /// </summary>
         public int ID { get; private set; }
+
+        /// <summary>
+        /// Maximum number of packet send retries
+        /// </summary>
+        public int MaxTries { get; set; }
+
+        public delegate void ReceiveEvent(byte[] data);
+        public event ReceiveEvent Receive;
 
         private ReliableConnection reliableConnection;
 
         private Dictionary<byte, GuaranteedPacket> guaranteedPackets;
-        private byte nextSendID = 0;
+        private byte nextSendID = 1;
 
         private Dictionary<byte, Timer> timers;
-        private int maxTries;
 
-        //private OrderedDictionary orderedPackets;
+        private OrderedDictionary orderedPackets;
         private byte nextExpectedID;
+        private byte[][] multiPacket = null;
 
         public Connection(ReliableConnection rc, EndPoint endPoint, int id)
         {
             reliableConnection = rc;
             EndPoint = endPoint;
             ID = id;
+            MaxTries = 10;
 
             guaranteedPackets = new Dictionary<byte, GuaranteedPacket>();
             timers = new Dictionary<byte, Timer>();
+            orderedPackets = new OrderedDictionary();
         }
 
-        public GuaranteedPacket AddPacket(byte[] packet, byte[] md5)
+        /// <summary>
+        /// Creates a guaranteed packet from the data passed in.
+        /// </summary>
+        /// <param name="packet">Packet data</param>
+        /// <param name="md5">MD5 hash of the packet data</param>
+        public void SendPacket(byte[] packet, byte[] md5)
         {
             GuaranteedPacket p = new GuaranteedPacket(nextSendID, packet, md5);
             guaranteedPackets.Add(nextSendID, p);
@@ -51,16 +72,69 @@ namespace Terralite
                 p.Tries++;
                 reliableConnection.Send(ID, p.ByteArray, p.Header);
 
-                if (p.Tries > maxTries)
+                if (p.Tries > MaxTries)
                     ClearPacket(p.PacketID);
             };
             timer.Start();
-
             timers.Add(p.PacketID, timer);
 
-            return p;
+            reliableConnection.Send(ID, p.ByteArray, p.Header);
         }
 
+        /// <summary>
+        /// Called when data is received from the managing ReliableConnection.
+        /// </summary>
+        /// <param name="buffer"></param>
+        public void ProcessData(byte[] buffer)
+        {
+            //pieces[0] = header
+            //pieces[1] = data
+            byte[][] pieces = buffer[0] != Packet.MULTI ? Utils.Split(buffer) : ProcessMulti(buffer);
+
+            if (OnPreReceive(pieces[0], pieces[1]))
+                OnReceive(pieces[1]);
+        }
+
+        /// <summary>
+        /// Handles storing multi-part packets and reconstructing when all
+        /// have arrived.
+        /// </summary>
+        /// <param name="buffer">One part of the whole packet</param>
+        /// <returns>null if all data hasn't been received, otherwise
+        /// Two byte arrays</returns>
+        private byte[][] ProcessMulti(byte[] buffer)
+        {
+            //Create a byte array of arrays for storing the parts of this multi packet
+            if (multiPacket == null)
+                multiPacket = new byte[buffer[1]][];
+
+            //Initialize this slot in the array and copy
+            multiPacket[buffer[2]] = new byte[buffer.Length - 3];
+            Array.Copy(buffer, 3, multiPacket[buffer[2]], 0, multiPacket[buffer[2]].Length);
+
+            //If we don't have all the packets, return null
+            for (int i = 0; i < multiPacket.Length; i++)
+                if (multiPacket[i] == null)
+                    return null;
+
+            MemoryStream ms = new MemoryStream();
+            for (int i = 0; i < multiPacket.Length; i++)
+                ms.Write(multiPacket[i], 0, multiPacket[i].Length);
+
+            byte[] whole = new byte[ms.Length];
+            ms.Read(whole, 0, whole.Length);
+
+            multiPacket = null;
+
+            return Utils.Split(whole);
+        }
+
+        /// <summary>
+        /// Called before <c>OnReceive</c> to do packet preprocessing.
+        /// </summary>
+        /// <param name="header">Header of packet</param>
+        /// <param name="data">Packet data to do preprocessing with</param>
+        /// <returns>Whether or not <c>OnReceive</c> needs to be called</returns>
         public bool OnPreReceive(byte[] header, byte[] data)
         {
             byte type = header[0];
@@ -98,7 +172,7 @@ namespace Terralite
                         else
                         {
                             //if already timeout waiting for this packet
-                            /*if (packetID < nextExpectedID)
+                            if (packetID < nextExpectedID)
                                 return false;
                             //if got next expected id
                             else if (packetID == nextExpectedID)
@@ -112,15 +186,14 @@ namespace Terralite
                                     orderedPackets.Remove(nextExpectedID);
                                     nextExpectedID = (byte)((nextExpectedID + 1) % byte.MaxValue);
 
-                                    OnReceive(op.Data, op.Data.Length);
+                                    OnReceive(op.Data);
                                 }
 
                                 return true;
                             }
 
                             orderedPackets.Add(packetID, new OrderedPacket(this, packetID, data, Packet.ORDER_TIMEOUT));
-                            return false;*/
-                            break;
+                            return false;
                         }
                     case Packet.ACK:
                         ClearPacket(packetID);
@@ -130,6 +203,19 @@ namespace Terralite
 
             Log("Didn't send packet id " + packetID);
             return false;
+        }
+
+        /// <summary>
+        /// Called when data has been received. Invokes all functions
+        /// stored in the Receive event.
+        /// </summary>
+        /// <param name="data">Data that was received</param>
+        /// <remarks>
+        /// Note this function does not do anything if <c>Receive</c> is <c>null</c>.
+        /// </remarks>
+        public void OnReceive(byte[] data)
+        {
+            Receive?.Invoke(data);
         }
 
         /// <summary>
@@ -145,11 +231,43 @@ namespace Terralite
             reliableConnection.Send(ID, ack);
         }
 
+        /// <summary>
+        /// Called by an OrderedPacket when its timeout has been reached.
+        /// </summary>
+        /// <param name="packetid">ID of packet that timed out</param>
+        protected void OnPacketTimeout(ushort packetid)
+        {
+            OrderedPacket op = (OrderedPacket)orderedPackets[(object)packetid];
+            orderedPackets.Remove(packetid);
+            nextExpectedID = (byte)((packetid + 1) % byte.MaxValue);
+
+            OnReceive(op.Data);
+        }
+
+        /// <summary>
+        /// Clears the specified packet id from sending.
+        /// </summary>
+        /// <param name="id">Packet id to clear</param>
         public void ClearPacket(byte id)
         {
             guaranteedPackets.Remove(id);
             timers[id].Dispose();
             timers.Remove(id);
+        }
+
+        /// <summary>
+        /// Clears all outgoing packets.
+        /// </summary>
+        public void ClearAllPackets()
+        {
+            foreach (KeyValuePair<byte, GuaranteedPacket> pair in guaranteedPackets)
+            {
+                timers[pair.Key].Dispose();
+                timers.Remove(pair.Key);
+            }
+
+            guaranteedPackets.Clear();
+            timers.Clear();
         }
 
         /// <summary>
@@ -160,6 +278,32 @@ namespace Terralite
         protected void Log<T>(T obj)
         {
             Console.WriteLine(obj);
+        }
+
+        /// <summary>
+        /// Class to hold data for handling an ordered packet
+        /// </summary>
+        private class OrderedPacket
+        {
+            public byte PacketID { get; set; }
+            public byte[] Data { get; set; }
+
+            private Connection connection;
+            private Timer timer;
+
+            public OrderedPacket(Connection c, byte packetID, byte[] packet, float timeout)
+            {
+                connection = c;
+                PacketID = packetID;
+                Data = packet;
+
+                timer = new Timer(timeout);
+                timer.Elapsed += (sender, args) =>
+                {
+                    connection.OnPacketTimeout(PacketID);
+                };
+                timer.Start();
+            }
         }
     }
 }
--- ReliableConnection.cs
+++ ReliableConnection.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Security.Cryptography;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Timers;
 using System.Threading;
 
 namespace Terralite
 {
     /// <summary>
-    /// ReliableClient extends the base Client functionality
-    /// by adding reliable sending. Packets that are transmitted incorrectly
+    /// ReliableConnection allows for sending data reliably or not.
+    /// If reliability is used, packets that are transmitted incorrectly
     /// will be resent. Packets that arrive out of order will be fixed.
     /// Packets can be send with a 100% guarantee they will arrive.
     /// </summary>
@@ -23,6 +21,11 @@ namespace Terralite
         /// Whether to log the extra debug information
         /// </summary>
         public bool Debug { get; set; }
+
+        /// <summary>
+        /// Whether to exit when a receive exception is thrown
+        /// </summary>
+        public bool ExitOnReceiveException { get; set; }
 
         /// <summary>
         /// Maximum number of send retries.
@@ -58,17 +61,14 @@ namespace Terralite
         /// </remarks>
         public bool UseOrdering { get; set; }
 
-        public delegate void ReceiveEvent(byte[] data, int numBytes);
-        public event ReceiveEvent Receive;
-
         protected const string DEFAULT_LOG = "log.txt";
-        protected const int DEFAULT_PORT = 10346;
 
         private Socket socket;
         private Thread receiveThread;
 
         private Dictionary<int, Connection> idToConnection;
         private Dictionary<EndPoint, Connection> epToConnection;
+        private int nextConnectionID = 1;
 
         /// <summary>
         /// Creates a <c>ReliableClient</c> object with the default
@@ -77,14 +77,15 @@ namespace Terralite
         public ReliableConnection() : this(DEFAULT_LOG) { }
 
         /// <summary>
-        /// Creates a <c>ReliableClient</c> object using <paramref name="logfile"/>
+        /// Creates a <c>ReliableConnection</c> object using <paramref name="logfile"/>
         /// as the log file.
         /// </summary>
         /// <param name="logfile">The logfile to use. Use <c>null</c> for
         /// no logging.</param>
-        /// <param name="port">The port to use</param>
-        public ReliableConnection(string logfile, int port = DEFAULT_PORT)
+        public ReliableConnection(string logfile)
         {
+            Debug = true;
+            ExitOnReceiveException = false;
             MaxRetries = 10;
             RetryInterval = 0.3f;
             UseMD5 = true;
@@ -92,11 +93,21 @@ namespace Terralite
 
             MD5 = MD5.Create();
             idToConnection = new Dictionary<int, Connection>();
+            epToConnection = new Dictionary<EndPoint, Connection>();
 
             if (logfile != null)
                 CreateLog(logfile);
 
-            CreateSocket(port);
+            CreateSocket();
+        }
+
+        /// <summary>
+        /// Whether this <c>ReliableConnection</c> is currently connected to any
+        /// remote endpoints
+        /// </summary>
+        public bool IsConnected
+        {
+            get { return idToConnection.Count > 0; }
         }
 
         /// <summary>
@@ -111,10 +122,9 @@ namespace Terralite
 
         /// <summary>
         /// Creates the socket object, sets it to non-blocking, and
-        /// binds it to a port.
+        /// binds it.
         /// </summary>
-        /// <param name="port">Port to try and bind to</param>
-        private void CreateSocket(int port)
+        private void CreateSocket()
         {
             Log("Creating socket...");
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -123,7 +133,7 @@ namespace Terralite
             try
             {
                 Log("Binding socket...");
-                socket.Bind(new IPEndPoint(IPAddress.Any, port));
+                socket.Bind(new IPEndPoint(IPAddress.Any, 0));
             }
             catch (SocketException e)
             {
@@ -135,19 +145,180 @@ namespace Terralite
             finally
             {
                 if (!socket.IsBound)
-                    socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+                {
+                    Log("Failed to bind socket");
 
-                Log("Socket bound to " + socket.LocalEndPoint);
+                    if (ExitOnReceiveException)
+                        Environment.Exit(1);
+                }
             }
         }
 
+        /// <summary>
+        /// Parses <paramref name="to"/> and creates an IPAddress from it.
+        /// </summary>
+        /// <param name="to">IP address or hostname to connect to</param>
+        /// <param name="port">Port to connect to</param>
+        public void Connect(string to, int port)
+        {
+            IPAddress address = null;
+            bool success = IPAddress.TryParse(to, out address);
+
+            if (!success)
+            {
+                try
+                {
+                    address = Dns.GetHostEntry(to).AddressList[0];
+                }
+                catch (Exception e)
+                {
+                    Log(e.Message);
+                    if (e.InnerException != null)
+                        Log(e.InnerException);
+                    Log(e.StackTrace);
+                }
+            }
+
+            if (address == null)
+            {
+                throw new TerraliteException("Invalid IP address/hostname: '" + to + "'.");
+            }
+
+            if (port < 0 || port >= 65536)
+            {
+                throw new TerraliteException("Invalid port: " + port + ". Must be between (0 - 65535)");
+            }
+
+            //successfully resolved the ip/hostname
+
+            if (receiveThread == null)
+            {
+                receiveThread = new Thread(new ThreadStart(ReceiveThread));
+                receiveThread.Start();
+            }
+
+            //create new connection
+            CreateConnection(address, port);
+        }
+
+        /// <summary>
+        /// Creates a new connection to <paramref name="address"/> at <paramref name="port"/>.
+        /// </summary>
+        /// <param name="address">The address to connect to</param>
+        /// <param name="port">The port to connect to</param>
+        private void CreateConnection(IPAddress address, int port)
+        {
+            try
+            {
+                EndPoint ep = new IPEndPoint(address, port);
+                Connection conn = new Connection(this, ep, nextConnectionID);
+                idToConnection.Add(nextConnectionID, conn);
+                epToConnection.Add(ep, conn);
+
+                Log("Connected to " + ep);
+
+                nextConnectionID = (nextConnectionID + 1) % int.MaxValue;
+            }
+            catch (Exception e)
+            {
+                Log(e.Message);
+                if (e.InnerException != null)
+                    Log(e.InnerException);
+                Log(e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Disconnects from a specific connection
+        /// </summary>
+        /// <param name="id">Connection id to remove</param>
+        /// <param name="remove">Whether to remove the Connection object from the dictionaries</param>
+        public void Disconnect(int id, bool remove = true)
+        {
+            if (!IsConnected)
+            {
+                if (Debug)
+                    Log("Not connected to any endpoints!");
+
+                return;
+            }
+
+            try
+            {
+                Send(id, Packet.DISCONNECT_PACKET);
+
+                //remove Connection from dictionaries
+                Connection conn = idToConnection[id];
+                conn.ClearAllPackets();
+
+                if (remove)
+                {
+                    idToConnection.Remove(id);
+                    epToConnection.Remove(conn.EndPoint);
+
+                    if (idToConnection.Count == 0)
+                    {
+                        receiveThread.Abort();
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
+                    }
+                }
+
+                Log("Disconnected from " + conn.EndPoint);
+            }
+            catch (Exception e)
+            {
+                Log(e.Message);
+                if (e.InnerException != null)
+                    Log(e.InnerException);
+                Log(e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Disconnects from all connections
+        /// </summary>
+        public void DisconnectAll()
+        {
+            foreach (KeyValuePair<int, Connection> pair in idToConnection)
+                Disconnect(pair.Key, false);
+
+            idToConnection.Clear();
+            epToConnection.Clear();
+        }
+
+        /// <summary>
+        /// Sends <paramref name="text"/> encoded as UTF8.
+        /// </summary>
+        /// <param name="id">Connection id to send to</param>
+        /// <param name="text">Message to send</param>
         public void Send(int id, string text)
         {
             Send(id, Encoding.UTF8.GetBytes(text));
         }
 
+        /// <summary>
+        /// Sends the <paramref name="buffer"/> to the connection ID specified.
+        /// </summary>
+        /// <param name="id">Connection id to send to</param>
+        /// <param name="buffer">A byte[] of data to send. If the buffer plus header
+        /// is bigger than 1400 bytes, it will be split across multiple calls to
+        /// Socket.SendTo</param>
+        /// <param name="header">Header to append to the buffer</param>
+        /// <remarks>
+        /// Note this function does not do anything if <c>IsConnected</c> is false or
+        /// the specified id does not exist.
+        /// </remarks>
         public void Send(int id, byte[] buffer = null, byte[] header = null)
         {
+            if (!IsConnected)
+            {
+                if (Debug)
+                    Log("Not connected to any endpoints!");
+
+                return;
+            }
+
             if (!idToConnection.ContainsKey(id))
             {
                 Log("ID doesn't exist in connections: " + id);
@@ -197,6 +368,7 @@ namespace Terralite
         /// <summary>
         /// Sends <paramref name="text"/> reliably and encoded as UTF8
         /// </summary>
+        /// <param name="id">Connection id to send to</param>
         /// <param name="text">Text to send</param>
         public void SendReliable(int id, string text)
         {
@@ -206,41 +378,87 @@ namespace Terralite
         /// <summary>
         /// Reliably sends <paramref name="packet"/>
         /// </summary>
+        /// <param name="id">Connection id to send to</param>
         /// <param name="packet">Data to send</param>
+        /// <remarks>
+        /// Note this function does not do anything if <c>IsConnected</c> is false or
+        /// the specified id does not exist.
+        /// </remarks>
         public void SendReliable(int id, byte[] packet)
         {
-            GuaranteedPacket gp = idToConnection[id].AddPacket(packet, MD5.ComputeHash(packet));
-            Send(id, gp.ByteArray, gp.Header);
+            if (!IsConnected)
+            {
+                if (Debug)
+                    Log("Not connected to any endpoints!");
+
+                return;
+            }
+
+            if (!idToConnection.ContainsKey(id))
+            {
+                Log("ID doesn't exist in connections: " + id);
+                return;
+            }
+
+            idToConnection[id].SendPacket(packet, MD5.ComputeHash(packet));
         }
 
         /// <summary>
-        /// Called before <c>OnReceive</c> to do packet preprocessing.
+        /// Receive thread function. Reads data from the socket and calls
+        /// <c>ProcessData</c>.
         /// </summary>
-        /// <param name="header">Header of packet</param>
-        /// <param name="data">Packet data to do preprocessing with</param>
-        /// <returns>Whether or not <c>OnReceive</c> needs to be called</returns>
-        protected virtual bool OnPreReceive(EndPoint endPoint, byte[] header, byte[] data)
+        private void ReceiveThread()
         {
-            Connection conn = epToConnection[endPoint];
-            return conn.OnPreReceive(header, data);
-        }
+            while (!IsConnected) { /* Wait for the connection to finish being created */}
 
-        protected void OnReceive(EndPoint endPoint, byte[] data)
-        {
+            byte[] buffer = null;
+            byte[] truncated = null;
+            EndPoint ep = new IPEndPoint(IPAddress.Any, 0);
 
+            while (IsConnected)
+            {
+                try
+                {
+                    buffer = new byte[Packet.MAX_SEND_SIZE];
+                    int numBytes = socket.ReceiveFrom(buffer, ref ep);
+                    truncated = new byte[numBytes];
+                    Array.Copy(buffer, truncated, truncated.Length);
+
+                    ProcessData(ep, truncated);
+                }
+                catch (SocketException)
+                {
+                    if (socket.Available == 0)
+                        continue;
+                }
+                catch (Exception e)
+                {
+                    Log(e.Message);
+                    if (e.InnerException != null)
+                        Log(e.InnerException);
+                    Log(e.StackTrace);
+
+                    if (ExitOnReceiveException)
+                        Environment.Exit(1);
+                }
+            }
         }
 
         /// <summary>
-        /// Called by an OrderedPacket when its timeout has been reached.
+        /// Checks is connected to <paramref name="ep"/> and calls ProcessData. Otherwise
+        /// do nothing.
         /// </summary>
-        /// <param name="packetid">ID of packet that timed out</param>
-        protected void OnPacketTimeout(ushort packetid)
+        /// <param name="ep">Connection EndPoint to use for data processing</param>
+        /// <param name="buffer">Data to process</param>
+        protected virtual void ProcessData(EndPoint ep, byte[] buffer)
         {
-            /*OrderedPacket op = (OrderedPacket)orderedPackets[(object)packetid];
-            orderedPackets.Remove(packetid);
-            nextExpectedID = (byte)((packetid + 1) % byte.MaxValue);
+            if (!epToConnection.ContainsKey(ep))
+            {
+                Log("Received data from an unknown remote: " + ep);
+                return;
+            }
 
-            OnReceive(op.Data, op.Data.Length);*/
+            epToConnection[ep].ProcessData(buffer);
         }
 
         /// <summary>
@@ -251,32 +469,6 @@ namespace Terralite
         protected void Log<T>(T obj)
         {
             Console.WriteLine(obj);
-        }
-
-        /// <summary>
-        /// Class to hold data for handling an ordered packet
-        /// </summary>
-        private class OrderedPacket
-        {
-            public byte PacketID { get; set; }
-            public byte[] Data { get; set; }
-
-            private ReliableConnection reliableClient;
-            private System.Timers.Timer timer;
-
-            public OrderedPacket(ReliableConnection rc, byte packetID, byte[] packet, float timeout)
-            {
-                reliableClient = rc;
-                PacketID = packetID;
-                Data = packet;
-
-                timer = new System.Timers.Timer(timeout);
-                timer.Elapsed += (sender, args) =>
-                {
-                    reliableClient.OnPacketTimeout(PacketID);
-                };
-                timer.Start();
-            }
         }
     }
 }
